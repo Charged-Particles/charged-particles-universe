4e40639f7f2f07733b54a30b210ec99066f0e160
uivlis <vladsilviufarcas@gmail.com>
2021-01-12 16:48:29 +0200

    Test Generic Wallet

---
+++ deploy/3_general.js
@@ -0,0 +1,51 @@
+const {
+  chainNameById,
+  chainIdByName,
+  saveDeploymentData,
+  getContractAbi,
+  getTxGasCost,
+  log,
+  presets,
+} = require("../js-helpers/deploy");
+
+module.exports = async (hre) => {
+    const { getNamedAccounts } = hre;
+    const { deployer, protocolOwner } = await getNamedAccounts();
+    const network = await hre.network;
+    const alchemyTimeout = 1;
+    const deployData = {};
+
+    const chainId = chainIdByName(network.name);
+
+    log('\n~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~');
+    log('Charged Particles LP: Generic - Contract Deployment');
+    log('~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n');
+
+    log('  Using Network: ', chainNameById(chainId));
+    log('  Using Accounts:');
+    log('  - Deployer:    ', deployer);
+    log('  - Owner:       ', protocolOwner);
+    log(' ');
+
+    log('\n  Deploying GenericWalletManager...')(alchemyTimeout);
+    const GenericWalletManager = await hre.ethers.getContractFactory('GenericWalletManager');
+    const GenericWalletManagerInstance = await GenericWalletManager.deploy();
+    const genericWalletManager = await GenericWalletManagerInstance.deployed();
+    deployData['GenericWalletManager'] = {
+      abi: getContractAbi('GenericWalletManager'),
+      address: genericWalletManager.address,
+      deployTransaction: genericWalletManager.deployTransaction,
+    }
+
+    // Display Contract Addresses
+    log('\n  Contract Deployments Complete!\n\n  Contracts:')(alchemyTimeout);
+    log('  - GenericWalletManager:  ', genericWalletManager.address);
+    log('     - Gas Cost:        ', getTxGasCost({ deployTransaction: genericWalletManager.deployTransaction }));
+
+    saveDeploymentData(chainId, deployData);
+    log('\n  Contract Deployment Data saved to "deployed" directory.');
+
+    log('\n~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n');
+};
+
+module.exports.tags = ['generic']
