7719c593e648aa94646941ae4df20be21e729de9
Rob Secord <robsecord@gmail.com>
2021-07-01 09:50:16 -0400

    Updates for Second Airdrop & Vesting

--- hardhat.config.js
+++ hardhat.config.js
@@ -76,8 +76,8 @@ module.exports = {
             },
         },
         kovan: {
-            url: `https://kovan.infura.io/v3/${process.env.INFURA_APIKEY}`,
-            // url: `https://eth-kovan.alchemyapi.io/v2/${process.env.ALCHEMY_APIKEY}`,
+            // url: `https://kovan.infura.io/v3/${process.env.INFURA_APIKEY}`,
+            url: `https://eth-kovan.alchemyapi.io/v2/${process.env.ALCHEMY_APIKEY}`,
             gasPrice: 10e9,
             blockGasLimit: 12400000,
             accounts: {
@@ -131,6 +131,8 @@ module.exports = {
         'Staking',
         'CommunityVault',
         'MerkleDistributor',
+        'MerkleDistributor2',
+        'VestingClaim',
       ],
     },
     namedAccounts: {
