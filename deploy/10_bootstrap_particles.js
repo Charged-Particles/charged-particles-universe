c0addb5b76eb7a9caf1bccc6decb249f6a6a4108
Your Name <scammi@gmail.com>
2022-04-15 18:35:09 -0300

    Init bootrap file, set steps

---
+++ deploy/10_bootstrap_particles.js
@@ -0,0 +1,13 @@
+/*
+  Create tokens to energize with
+    - Create 1155 and 721 and set owner as test wallet.
+    - create 2 ecr721
+    - create 1 erc1155 1 balance
+    - create 1 erc1144 100 balance
+
+  - Set permissions on new NFTs to the charged particle address
+     - get charged sol address
+     - set permissions for tokens
+     
+  - Mint particles and energize
+*/
