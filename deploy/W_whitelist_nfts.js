4d40a84c9998fcabae6454b09d7c573450a1ba6c
Rob Secord <robsecord@gmail.com>
2022-05-03 13:41:49 -0400

    Updates for whitelisting

--- deploy/W_whitelist_nfts.js
+++ deploy/W_whitelist_nfts.js
@@ -15,6 +15,26 @@ const {
 
 const _ = require('lodash');
 
+
+const _WHITELISTED_CONTRACTS = {
+  '1': [
+    "0x3cd2410eaa9c2dce50af6ccab72dc93879a09c1f",
+    "0x63174fa9680c674a5580f7d747832b2a2133ad8f",
+    "0x929167191ca41a4753eda357bb6e5ad6f15fb89b",
+    "0xc0cb81c1f89ab0873653f67eea42652f13cd8416",
+    "0xccc441ac31f02cd96c153db6fd5fe0a2f4e6a68d",
+    "0xcd2ba94e435e536dc48648eab2f4f1db257bc64c",
+    "0xd07dc4262bcdbf85190c01c996b4c06a461d2430",
+    "0xd86898728aa9921101515a38b7d15d16c682e8ce",
+    "0xdfe3ac769b2d8e382cb86143e0b0b497e1ed5447",
+  ],
+  '137': [
+    "0x4ed360c8725d3a63f564f8484a582d0a7cecea7a",
+    "0xe2a9b15e283456894246499fb912cce717f83319",
+  ]
+};
+
+
 module.exports = async (hre) => {
     const { ethers, getNamedAccounts } = hre;
     const { deployer, protocolOwner } = await getNamedAccounts();
@@ -24,7 +44,7 @@ module.exports = async (hre) => {
     const {isProd, isHardhat} = chainTypeById(chainId);
     const alchemyTimeout = isHardhat ? 0 : (isProd ? 10 : 7);
 
-    if (chainId !== 42) { return; } // Kovan only
+    // if (chainId !== 42) { return; } // Kovan only
 
     const ddChargedSettings = getDeployData('ChargedSettings', chainId);
 
@@ -44,12 +64,14 @@ module.exports = async (hre) => {
 
 
     //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-    // Setup Proton
+    // Enable Contracts for Charging
     //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
-    // await executeTx('1-a', `ChargedSettings: Enabling External NFT for "blobwatanabe"`, async () =>
-    //   await chargedSettings.enableNftContracts(['0x36DF5DFe089765c59A8a80527A1Ff1Aae0Af3f99'])
-    // );
+    if (_WHITELISTED_CONTRACTS[chainId].length) {
+      await executeTx('1-a', `ChargedSettings: Enabling ${_WHITELISTED_CONTRACTS[chainId].length} Contracts for Chain ID: ${chainId}`, async () =>
+        await chargedSettings.enableNftContracts(_WHITELISTED_CONTRACTS[chainId])
+      );
+    }
 
     // await executeTx('1-a', `ChargedSettings: Enabling External NFT for "crypto-j-42"`, async () =>
     //   await chargedSettings.enableNftContracts(['0x9C97e093A01061C7FDc7ED6c1eeCA0C20C3d294F'])
