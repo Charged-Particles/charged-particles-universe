fc2de3237eb2e80388577833829132b849461549
scammi <scammi@gmail.com>
2023-02-17 12:26:06 -0300

    check balance

--- test/staking/RewardProgram.test.js
+++ test/staking/RewardProgram.test.js
@@ -11,32 +11,34 @@ const {
 } = require('../../js-helpers/deploy');
 
 describe('Reward program', function () {
-  let rewardProgram;
+  let rewardProgram, ionx, protocolOwnerSigner;
+
+  before(async () => {
+    const chainId = await getChainId();
+    const ddIonx = getDeployData('Ionx', chainId);
+    const Ionx = await ethers.getContractFactory('Ionx');
+    ionx = await Ionx.attach(ddIonx.address);
+
+    const { protocolOwner } = await getNamedAccounts();
+    protocolOwnerSigner = protocolOwner;
+  });
+
   beforeEach(async function () {
     const ddRewardProgram = getDeployData('RewardProgram');
     const RewardProgram = await ethers.getContractFactory('RewardProgram');
     rewardProgram = RewardProgram.attach(ddRewardProgram.address); 
-
-    // instantiate ionx erc20s
   });
 
   it('should be deployed', async () =>{
     expect(rewardProgram.address).to.not.equal(0);
-
     const rewardData = await rewardProgram.getProgramData();
     expect(rewardData.totalStake).to.equal(0);
   });
 
   describe('Funds reward pool', () => {
     it('Has balance', async() => {
-      const chainId = await getChainId();
-      const ddIonx = getDeployData('Ionx', chainId);
-      const Ionx = await ethers.getContractFactory('Ionx');
-      const ionx = await Ionx.attach(ddIonx.address);
-
-      const { protocolOwner} = await getNamedAccounts();
-      const balance = await ionx.balanceOf(protocolOwner);
-
+      const balance = await ionx.balanceOf(protocolOwnerSigner);
+      expect(balance).to.gt(0)
     });
     
     it.skip('Deposits IONX into the reward pool', async () => {
