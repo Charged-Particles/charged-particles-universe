51804697116f35c82308ba2f16430ef64872bb41
scammi <scammi@gmail.com>
2023-07-13 10:41:20 -0300

    Added test update

--- test/staking/RewardProgram.test.js
+++ test/staking/RewardProgram.test.js
@@ -64,7 +64,7 @@ describe('Reward program', function () {
     rewardWalletManagerMock = await deployMockContract(deployerSigner, walletManager.abi);
 
     await rewardProgramDeployerSigner.setUniverse(await deployerSigner.getAddress()).then(tx => tx.wait());
-    programData = await rewardProgram.getProgramData();
+    programData = await rewardProgramDeployerSigner.getProgramData();
 
     // Instantiate universe
     const Universe= await ethers.getContractFactory('Universe');
@@ -168,8 +168,8 @@ describe('Reward program', function () {
     });
 
     it('Verifies simple lepton reward calculation', async () => {
-      const contractAddress = '0x5d183d790d6b570eaec299be432f0a13a00058a7';
       const tokenId = 6
+      const contractAddress = '0x5d183d790d6b570eaec299be432f0a13a00058a7';
       const leptonMultiplier = 200; // x2
       const principal = 1000000;
       const leptonId = 89;
@@ -214,27 +214,40 @@ describe('Reward program', function () {
       const stakeInfoCases = [
         {
           amount: 10,
-          blocksUntilLeptonDeposit: 0,
+          blocksUntilLeptonDeposit: 1,
           blocksUntilCalculation: 500,
           leptonStakeMultiplier: 200,
           generatedChargedBeforeLeptonRelease: 1000000,
           generatedChargeAfterLeptonRelease: 1000000,
           blocksUntilLeptonRelease: 500,
-          expectedReward: '1499500',
+          expectedReward: '1499000000000000000',
           tokenId: 42,
           description: 'Lepton deposited half of the reward length'
         },
+        
         {
           amount: 10,
-          blocksUntilLeptonDeposit: 0,
+          blocksUntilLeptonDeposit: 1,
           blocksUntilCalculation: 1000,
           leptonStakeMultiplier: 200,
           generatedChargedBeforeLeptonRelease: 1,
           generatedChargeAfterLeptonRelease: 1000000,
           blocksUntilLeptonRelease: 0,
-          expectedReward: '1998000',
+          expectedReward: '1997000000000000000',
           tokenId: 43,
           description: 'Unstake with deposited lepton inside'
+        },
+        {
+          amount: ethers.utils.parseUnits('1.0', 6),
+          blocksUntilLeptonDeposit: 0,
+          blocksUntilCalculation: 100000,
+          leptonStakeMultiplier: 100,
+          generatedChargedBeforeLeptonRelease: 0,
+          generatedChargeAfterLeptonRelease: ethers.utils.parseUnits('1.0', 6),
+          blocksUntilLeptonRelease: 0,
+          expectedReward: '1000000000000000000',
+          tokenId: 44,
+          description: 'Base multiplier 1x, testing returned decimals'
         },
       ];
 
@@ -244,6 +257,7 @@ describe('Reward program', function () {
         await leptonMock.mock.ownerOf.returns(receiverAddress);
         await leptonMock.mock.isApprovedForAll.returns(true);
         await ionxMock.mock.transfer.returns(true);
+        await ionxMock.mock.decimals.returns(6);
         await ionxMock.mock.balanceOf.returns(ethers.utils.parseEther('100'));
 
         await rewardProgramDeployerSigner.registerAssetDeposit(
@@ -253,15 +267,16 @@ describe('Reward program', function () {
           stakeInfoCases[i].amount
         ).then(tx => tx.wait());
 
-        await mineBlocks(stakeInfoCases[i].blocksUntilLeptonDeposit);
-        
-        await rewardProgramDeployerSigner.registerNftDeposit(
-          leptonMock.address,
-          stakeInfoCases[i].tokenId,
-          leptonMock.address,
-          i + stakeInfoCases[i].tokenId,
-          0
-        ).then(tx => tx.wait());
+        if (stakeInfoCases[i]?.blocksUntilLeptonDeposit) {
+          await mineBlocks(stakeInfoCases[i].blocksUntilLeptonDeposit);
+          await rewardProgramDeployerSigner.registerNftDeposit(
+            leptonMock.address,
+            stakeInfoCases[i].tokenId,
+            leptonMock.address,
+            i + stakeInfoCases[i].tokenId,
+            0
+          ).then(tx => tx.wait());
+        }
 
         if (stakeInfoCases[i]?.blocksUntilLeptonRelease) {
           await mineBlocks(stakeInfoCases[i].blocksUntilLeptonRelease);
